5aebab009c63c86887cb6ce594184c1f5d2fb3c8
corix <rico.c.marcelo@gmail.com>
2012-04-20 22:33:47 -0500

    bugs fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
+  end
+
+  def search
     user = User.find_by_username(params[:username])
     session[:user] = user
+    redirect_to :action => 'index'
   end
 end
